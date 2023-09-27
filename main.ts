a7ed161302a0b763de447abab6b38e635bc10bca
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-09-27 17:38:27 -0400

    Python change 12

--- main.ts
+++ main.ts
@@ -13,7 +13,7 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-  temperature =  input.temperature() * 273.15
+  temperature = input.temperature() + 273.15
   temperature = Math.round(temperature)
 
   basic.showString('temperature is: ' + temperature.toString() + 'K')
