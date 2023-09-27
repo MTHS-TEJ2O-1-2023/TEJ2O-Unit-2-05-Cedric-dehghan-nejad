da6688b4360b1b78c185a6b4af3d73df41c14966
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-09-27 18:11:26 -0400

    Python change 15

--- main.ts
+++ main.ts
@@ -5,7 +5,6 @@
  * This program ...
  * /// this program tell the temperature in kelvin
 */
-
 // our variable for a random number
 let temperatureCelsius: number
 let temperatureKelvin: number
@@ -15,7 +14,7 @@ basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
   temperatureCelsius = input.temperature()
-  temperatureKelvin = temperatureCelsius  + 273.15
+  temperatureKelvin = temperatureCelsius + 273.15
   temperatureKelvin = Math.round(temperatureKelvin)
 
   basic.showString('temperature is: ' + (temperatureKelvin).toString() + 'K')
